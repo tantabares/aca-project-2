1733f55f446b3ba53a2c4104d1ce4296d705a09d
Jhonathan Tabares <jhonathan.tabares@somoglobal.com>
2020-03-23 17:02:11 -0500

    Added scroll styles

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-const BODY = document.getElementsByTagName('body')[0];
+const BODY = document.body;
 const THEME_BUTTONS = document.getElementsByClassName('gif-theme-button')
+const THEME_DROPDOWN = document.getElementById('gifOS_dropdown')
 
-let changeTheme = function(e) {
+let changeTheme = function() {
     let currentTheme = BODY.dataset.theme
     let newTheme = this.dataset.val
     let newLogo = this.dataset.logo
@@ -14,17 +15,40 @@ let changeTheme = function(e) {
     }
 }
 
-for (let i = 0; i < THEME_BUTTONS.length; i++) {
-    THEME_BUTTONS[i].addEventListener('click', changeTheme, false)
-}
-
-document.getElementById('gifOS_dropdown').addEventListener('click', function(e) {
+let toggleThemeDropdown = function() {
     document.getElementById('gifOS_theme-options').classList.toggle('show')
-    this.classList.toggle('show')
+    THEME_DROPDOWN.classList.toggle('show')
 
     let buttons = document.querySelectorAll('.gif-button__main.dropd')
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.toggle('clicked')
+        buttons[i].blur()
     }
+}
+
+for (let i = 0; i < THEME_BUTTONS.length; i++) {
+    THEME_BUTTONS[i].addEventListener('click', changeTheme, false)
+}
+
+THEME_DROPDOWN.addEventListener('click', function(e) {
+    toggleThemeDropdown()
+}, false)
+
+window.onscroll = function() {
+    if (window.pageYOffset > 25) {
+        document.getElementById('gifOS_header-cont').classList.add('bottom-line')
+    }
+
+    if (window.pageYOffset < 25) {
+        document.getElementById('gifOS_header-cont').classList.remove('bottom-line')
+    }
+};
+
+document.addEventListener('click', function(e){ 
+    if (e.target.closest('#gifOS_dropdown')) 
+        return
+    else 
+        if (THEME_DROPDOWN.classList.contains('show'))
+            toggleThemeDropdown()
 }, false)
